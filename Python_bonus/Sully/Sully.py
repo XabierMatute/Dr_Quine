fea267f6548956b89cfd6131e6633658470c4271
xmatute- <xmatute-@student.42urduliz.com>
2025-09-17 14:20:00 +0200

    mas guay con la x

--- Python_bonus/Sully/Sully.py
+++ Python_bonus/Sully/Sully.py
@@ -4,14 +4,15 @@ import subprocess
 
 # This program creates copies of itself with decremented counter
 i = 5
+x = i - 1
 source = '#!/usr/bin/env python3\nimport os\nimport subprocess\n\n# This program creates copies of itself with decremented counter\ni = %d\nsource = %r\n\nif i >= 0:\n    filename = f"Sully_{i - 1}.py"\n    with open(filename, "w") as f:\n        f.write(source %% (i - 1, source))\n    \n    if i > 0:\n        # Make executable\n        os.chmod(filename, 0o755)\n        # Run the new program\n        subprocess.run(["python3", filename])\n'
 
 if i >= 0:
-    filename = f"Sully_{i - 1}.py"
+    filename = f"Sully_{x}.py"
     with open(filename, "w") as f:
-        f.write(source % (i - 1, source))
+        f.write(source % (x, source))
     
-    if i > 0:
+    if x >= 0:
         # Make executable
         os.chmod(filename, 0o755)
         # Run the new program
