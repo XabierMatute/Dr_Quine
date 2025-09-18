feeaea40e0f70a75334a11203aebedec23c677ea
xmatute- <xmatute-@student.42urduliz.com>
2025-09-18 12:05:01 +0200

    collen s wip

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,32 +1,21 @@
+section .data
+    s db "section .data    s db %c%s%c, 0", 0
+
 section .text
-global main
+    global main
+    extern printf
 
 main:
-    mov rax, 1        ; write(
-    mov rdi, 1        ;   STDOUT_FILENO,
-    lea rsi, [rel msg];   msg,
-    mov rdx, msglen   ;   sizeof(msg)
-    syscall           ; );
+    push rbp
+    mov rbp, rsp
     
-    mov rax, 0        ; return 0
+    mov rdi, s      ; format string
+    mov rsi, 34     ; ASCII code for "
+    mov rdx, s      ; string content
+    mov rcx, 34     ; ASCII code for "
+    mov rax, 0      ; number of vector registers used
+    call printf
+    
+    mov rax, 0      ; return value
+    pop rbp
     ret
-
-section .rodata
-    msg: db "section .text", 10
-         db "global main", 10, 10
-         db "main:", 10
-         db "    mov rax, 1        ; write(", 10
-         db "    mov rdi, 1        ;   STDOUT_FILENO,", 10
-         db "    lea rsi, [rel msg];   msg,", 10
-         db "    mov rdx, msglen   ;   sizeof(msg)", 10
-         db "    syscall           ; );", 10, 10
-         db "    mov rax, 0        ; return 0", 10
-         db "    ret", 10, 10
-         db "section .rodata", 10
-
-         db "    msg: db ", 34, msg, 34, ", 10", 10
-
-         db 10, 10
-         db "    msglen: equ $ - msg", 10
-
-    msglen: equ $ - msg
