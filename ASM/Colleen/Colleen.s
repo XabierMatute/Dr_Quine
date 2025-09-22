958b00f2f432ef8e6308286b03673bae1becc655
xmatute- <xmatute-@student.42urduliz.com>
2025-09-22 16:00:43 +0200

    lio

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,21 +1,65 @@
-section .data
-    s db "section .data    s db %c%s%c, 0", 0
-
 section .text
     global main
     extern printf
+
+decapush:
+    push 10
+    push 10
+    push 10
+    push 10
+    push 10
+    push 10
+    push 10
+    push 10
+    push 10
+    push 10
+    ret
+
+print_code:
+    push rbp
+    mov rbp, rsp
+
+
+    
+    mov rdi, s
+    mov rsi, 10
+    mov rdx, 10
+    mov rcx, 10
+    mov r8, 10
+    mov r9, 10
+
+    call decapush
+    ; push 14     ;1
+    ; push 20     ;1
+    mov rax, 0      ; number of vector registers used
+
+    ; mov rax, 1      ; number of vector registers used
+    call printf
+
+    add rsp, 16
+    add rsp, 16
+    add rsp, 16
+    add rsp, 16
+    add rsp, 16
+
+    pop rbp
+    ret
+
+; This program will print its own source when run.
 
 main:
     push rbp
     mov rbp, rsp
     
-    mov rdi, s      ; format string
-    mov rsi, 34     ; ASCII code for "
-    mov rdx, s      ; string content
-    mov rcx, 34     ; ASCII code for "
-    mov rax, 0      ; number of vector registers used
-    call printf
+    ; This function prints the source code
+    call print_code
     
     mov rax, 0      ; return value
     pop rbp
     ret
+
+section .data
+    s db "section .text%c    global main%c    extern printf%c%cprint_code:%c    push rbp%c"
+;     s db "1%c2%c%c%c%c6%c"
+; section .data
+;     s db "test: %d %d %d %d %d (%d) %d %d %d", 10, 0
