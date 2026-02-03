25b3b1ba2b8eeaf2612ac8a2b89c709cdc135799
xmatute- <xmatute-@student.42urduliz.com>
2026-02-03 09:30:53 +0100

    Colleen.s done

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -7,11 +7,11 @@ print_code:
     mov rbp, rsp
     
     mov rdi, code
-    mov rsi, 34
-    mov rdx, code
-    mov rcx, 34
+    mov rsi, 10
+    mov rdx, 34
+    mov rcx, code
 
-    mov rax, 0      ; number of vector registers used
+    mov rax, 0
 
     call printf
 
@@ -27,9 +27,9 @@ main:
     ; This function prints the source code
     call print_code
     
-    mov rax, 0      ; return value
+    mov rax, 0
     pop rbp
     ret
 
 section .data
-   code db"section .text\n    global main\n    extern printf\n\nprint_code:%c    push rbp%c    mov rbp, rsp%c    %cmov rdi, code%c    mov rsi, 34%c    mov rdx, code%c    mov rcx, 34%c%c    mov rax, 0      ; number of vector registers used%c%c    call printf%c%c    pop rbp%c    ret%c%c; This program will print its own source when run.%c%cmain:%c    push rbp%c    mov rbp, rsp%c    %c; This function prints the source code%c    call print_code%c    %cmov rax, 0      ; return value%c    pop rbp%c    ret%c%csection .data% c   code db%c%s%c"
+   code db"section .text%1$c    global main%1$c    extern printf%1$c%1$cprint_code:%1$c    push rbp%1$c    mov rbp, rsp%1$c    %1$c    mov rdi, code%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    mov rcx, code%1$c%1$c    mov rax, 0%1$c%1$c    call printf%1$c%1$c    pop rbp%1$c    ret%1$c%1$c; This program will print its own source when run.%1$c%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c    %1$c    ; This function prints the source code%1$c    call print_code%1$c    %1$c    mov rax, 0%1$c    pop rbp%1$c    ret%1$c%1$csection .data%1$c   code db%2$c%3$s%2$c"
