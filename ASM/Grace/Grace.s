8b21b669f81cc5c9b5d300fbf7c0fd3bdaef40dd
xmatute- <xmatute-@student.42urduliz.com>
2026-02-17 15:35:37 +0100

    gracia galean

--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -20,7 +20,7 @@
     pop rbp
 %endmacro
 
-%macro CLOSE 3
+%macro CLOSE 0
     mov rax, 3
     mov rdi, rbx
     syscall
@@ -39,4 +39,4 @@ main:
 
 section .rodata
     fn: db "Grace_kid.s", 0
-    code: db "%3$cmacro OPEN 1%1$c    mov rax, 2%1$c    lea rdi, [rel %3$c1]%1$c    mov rsi, 577%1$c    mov rdx, 0664o%1$c    syscall%1$c    mov rbx, rax%1$c%3$cendmacro%1$c%1$c%3$cmacro PRINT_TO_FILE 2%1$c    push rbp%1$c    mov rdi, rbx%1$c    mov rsi, code%1$c    mov rdx, 10%1$c    mov rcx, 34%1$c    mov r8, 37%1$c    mov r9, code%1$c    mov rax, 0%1$c    call dprintf wrt ..plt%1$c    pop rbp%1$c%3$cendmacro%1$c%1$c%3$cmacro CLOSE 3%1$c    mov rax, 3%1$c    mov rdi, rbx%1$c    syscall%1$c%3$cendmacro%1$c%1$csection .text%1$c    global main%1$c    extern dprintf%1$c%1$c; This program will create a copy of its own source when run.%1$c%1$cmain:%1$c    OPEN fn%1$c    PRINT_TO_FILE code, 0%1$c    CLOSE%1$c%1$csection .rodata%1$c    fn: db %2$cGrace_kid.s%2$c, 0%1$c    code: db %2$c%4$s%2$c, 0", 0
+    code: db "%3$cmacro OPEN 1%1$c    mov rax, 2%1$c    lea rdi, [rel %3$c1]%1$c    mov rsi, 577%1$c    mov rdx, 0664o%1$c    syscall%1$c    mov rbx, rax%1$c%3$cendmacro%1$c%1$c%3$cmacro PRINT_TO_FILE 2%1$c    push rbp%1$c    mov rdi, rbx%1$c    mov rsi, code%1$c    mov rdx, 10%1$c    mov rcx, 34%1$c    mov r8, 37%1$c    mov r9, code%1$c    mov rax, 0%1$c    call dprintf wrt ..plt%1$c    pop rbp%1$c%3$cendmacro%1$c%1$c%3$cmacro CLOSE 0%1$c    mov rax, 3%1$c    mov rdi, rbx%1$c    syscall%1$c%3$cendmacro%1$c%1$csection .text%1$c    global main%1$c    extern dprintf%1$c%1$c; This program will create a copy of its own source when run.%1$c%1$cmain:%1$c    OPEN fn%1$c    PRINT_TO_FILE code, 0%1$c    CLOSE%1$c%1$csection .rodata%1$c    fn: db %2$cGrace_kid.s%2$c, 0%1$c    code: db %2$c%4$s%2$c, 0", 0
