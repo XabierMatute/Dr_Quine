f4e0392a35fe53022ebe1533f574992e13def34b
xmatute- <xmatute-@student.42urduliz.com>
2026-02-27 15:14:31 +0100

    done

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -20,7 +20,6 @@ main:
     mov rax, [rel i]
     dec rax
 
-    ; --- crear el archivo .s ---
     lea rdi, [rsp]
     mov rsi, 64
     lea rdx, [rel filename]
@@ -49,7 +48,6 @@ main:
     mov rdi, [rel fd]
     call close
 
-    ; --- nasm ---
     mov  rax, [rel i]
     dec  rax
     lea  rdi, [rsp]
@@ -63,7 +61,6 @@ main:
     lea  rdi, [rsp]
     call system
 
-    ; --- gcc ---
     mov  rax, [rel i]
     dec  rax
     lea  rdi, [rsp]
@@ -77,7 +74,6 @@ main:
     lea  rdi, [rsp]
     call system
 
-    ; --- ejecutar ---
     mov  rax, [rel i]
     dec  rax
     lea  rdi, [rsp]
@@ -107,8 +103,8 @@ section .data
 
 section .rodata
     i:            dq 5
-    code: db "section .text%2$c    global main%2$c    extern printf%2$c    extern snprintf%2$c    extern dprintf%2$c    extern open%2$c    extern close%2$c%2$cmain:%2$c    push rbp%2$c    mov rbp, rsp%2$c    sub rsp, 64%2$c%2$c    mov rax, [rel i]%2$c    cmp rax, 0%2$c    jl .negative%2$c%2$c.positive:%2$c    mov rax, [rel i]%2$c    dec rax%2$c%2$c    lea rdi, [rsp]%2$c    mov rsi, 64%2$c    lea rdx, [rel fmt_filename]%2$c    mov rcx, rax%2$c    xor eax, eax%2$c    call snprintf%2$c%2$c    lea rdi, [rsp]%2$c    mov rsi, 0x241%2$c    mov rdx, 0644o%2$c    xor eax, eax%2$c    call open%2$c%2$c    mov [rel fd], rax%2$c%2$c    mov rdi, [rel fd]%2$c    lea rsi, [rel code]%2$c    mov rdx, [rel i]%2$c    mov rcx, 10%2$c    dec rdx%2$c    mov r8,  34%2$c    lea r9,  [rel code]%2$c    xor eax, eax%2$c    call dprintf%2$c%2$c    mov rdi, [rel fd]%2$c    call close%2$c%2$c    jmp .done%2$c%2$c.negative:%2$c    jmp .done%2$c%2$c%2$c.done:%2$c    xor eax, eax%2$c    add rsp, 64%2$c    pop rbp%2$c    ret%2$c    %2$c%2$csection .data%2$c    fd:           dq 0%2$c%2$csection .rodata%2$c    i:            dq %1$d%2$c    code: db %3$c%4$s%3$c, 0%2$c    fmt_filename: db %3$cSully_%%i.s%3$c, 0", 0
+    code: db "section .text%2$c    global main%2$c    extern printf%2$c    extern snprintf%2$c    extern dprintf%2$c    extern open%2$c    extern close%2$c    extern system%2$c%2$cmain:%2$c    push rbp%2$c    mov rbp, rsp%2$c    sub rsp, 64%2$c%2$c    mov rax, [rel i]%2$c    cmp rax, 0%2$c    jl .negative%2$c%2$c.positive:%2$c    mov rax, [rel i]%2$c    dec rax%2$c%2$c    lea rdi, [rsp]%2$c    mov rsi, 64%2$c    lea rdx, [rel filename]%2$c    mov rcx, rax%2$c    xor eax, eax%2$c    call snprintf%2$c%2$c    lea rdi, [rsp]%2$c    mov rsi, 0x241%2$c    mov rdx, 0644o%2$c    xor eax, eax%2$c    call open%2$c%2$c    mov [rel fd], rax%2$c%2$c    mov rdi, [rel fd]%2$c    lea rsi, [rel code]%2$c    mov rdx, [rel i]%2$c    mov rcx, 10%2$c    dec rdx%2$c    mov r8,  34%2$c    lea r9,  [rel code]%2$c    xor eax, eax%2$c    call dprintf%2$c%2$c    mov rdi, [rel fd]%2$c    call close%2$c%2$c    mov  rax, [rel i]%2$c    dec  rax%2$c    lea  rdi, [rsp]%2$c    mov  rsi, 64%2$c    lea  rdx, [rel cmd_nasm]%2$c    mov  rcx, rax%2$c    mov  r8,  rax%2$c    xor  eax, eax%2$c    call snprintf%2$c%2$c    lea  rdi, [rsp]%2$c    call system%2$c%2$c    mov  rax, [rel i]%2$c    dec  rax%2$c    lea  rdi, [rsp]%2$c    mov  rsi, 64%2$c    lea  rdx, [rel cmd_cc]%2$c    mov  rcx, rax%2$c    mov  r8,  rax%2$c    xor  eax, eax%2$c    call snprintf%2$c%2$c    lea  rdi, [rsp]%2$c    call system%2$c%2$c    mov  rax, [rel i]%2$c    dec  rax%2$c    lea  rdi, [rsp]%2$c    mov  rsi, 64%2$c    lea  rdx, [rel cmd_run]%2$c    mov  rcx, rax%2$c    xor  eax, eax%2$c    call snprintf%2$c%2$c    lea  rdi, [rsp]%2$c    call system%2$c%2$c    jmp .done%2$c%2$c.negative:%2$c    jmp .done%2$c%2$c.done:%2$c    xor eax, eax%2$c    add rsp, 64%2$c    pop rbp%2$c    ret%2$c    %2$c%2$csection .data%2$c    fd:           dq 0%2$c%2$csection .rodata%2$c    i:            dq %1$d%2$c    code: db %3$c%4$s%3$c, 0%2$c    filename: db %3$cSully_%%i.s%3$c, 0%2$c    cmd_nasm: db %3$cnasm -f elf64 Sully_%%d.s -o Sully_%%d.o%3$c, 0%2$c    cmd_cc:   db %3$cgcc Sully_%%d.o -o Sully_%%d -no-pie%3$c, 0%2$c    cmd_run:  db %3$c./Sully_%%d%3$c, 0", 0
     filename: db "Sully_%i.s", 0
-    cmd_nasm: db "nasm -f elf64 Sully_%lld.s -o Sully_%lld.o", 0
-    cmd_cc:   db "gcc Sully_%lld.o -o Sully_%lld -no-pie", 0
-    cmd_run:  db "./Sully_%lld", 0
+    cmd_nasm: db "nasm -f elf64 Sully_%d.s -o Sully_%d.o", 0
+    cmd_cc:   db "gcc Sully_%d.o -o Sully_%d -no-pie", 0
+    cmd_run:  db "./Sully_%d", 0
